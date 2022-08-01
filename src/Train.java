822020703485bfbf9a9854a5447b13e7fcad99c7
Nikita Plotnitskiy <NikitaPl131@gmail.com>
2022-08-01 21:30:15 +0300

    First Commit

---
+++ src/Train.java
@@ -0,0 +1,10 @@
+import static java.lang.Integer.sum;
+
+public class Train {
+    public static void main (String[] args) {
+        int[] array = {5, 4, 6, 9, 8};
+        for (int i = 0; i<5; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
