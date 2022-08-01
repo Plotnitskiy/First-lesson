e3303cb330e6a26d432f9db86b7a9ad6d8c16c92
Nikita Plotnitskiy <NikitaPl131@gmail.com>
2022-08-01 22:40:35 +0300

    First Commit

---
+++ src/Train1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Train1 {
+    public static void main(String[] args) {
+        // посчитаем длину окружности, если ее радиус считывается с клаиватуры
+        Scanner sc = new Scanner(System.in);
+        double d; // диаметр окружности
+        double pi;
+        System.out.println("Input d");
+        d = sc.nextDouble();
+        System.out.println("Input pi");
+        pi = sc.nextDouble();
+        d = 5;
+        pi = 3.14;
+        double circle = pi * d;
+        System.out.println(circle);
+        // выводит Input d. Не знаю, как эту задачку решить
+    }
+}
