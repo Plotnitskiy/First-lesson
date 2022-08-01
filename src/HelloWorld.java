b186f1a7447b683cf40608a81c1ee75f214bcc63
Nikita Plotnitskiy <NikitaPl131@gmail.com>
2022-08-01 21:54:40 +0300

    First Commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Nikita");
-
+        // напишем программу, которая вычисляет площадь треугольника по рандомным параметрам
+        // умножаем две стороны, делим пополам и умножаем на синус угла
+        int a = 3;
+        int b = 4;
+        int x = 30;
+        double square = (a * b / 2 * Math.sin(x));
+        System.out.println(square);
     }
 }
--- src/Train.java
+++ src/Train.java
@@ -3,6 +3,7 @@ import static java.lang.Integer.sum;
 public class Train {
     public static void main (String[] args) {
         int[] array = {5, 4, 6, 9, 8};
+        // нужно прописать вертикально в ряд данные числа
         for (int i = 0; i<5; i++){
             System.out.println(array[i]);
         }
