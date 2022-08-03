ec73caa094444a761e2c0be6d7adf6fb4fba084b
Nikita Plotnitskiy <NikitaPl131@gmail.com>
2022-08-03 18:12:08 +0300

    Высокосный год или нет?

---
+++ src/LeapYear.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        System.out.println("Input year");
+        year = sc.nextInt();
+        if(year % 4 == 0 || year % 400 == 0) {
+            System.out.println("There are 366 days in a year");
+        } else if (year % 100 == 0) {
+            System.out.println("There are 365 days in a year");
+        }else {
+            System.out.println("There are 365 days in a year");
+        }
+    }
+}
