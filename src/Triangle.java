f82e3379f56cda66d61a0d989d777e7221359882
Nikita Plotnitskiy <NikitaPl131@gmail.com>
2022-08-03 18:25:58 +0300

    Может ли существовать треугольник при следующих сторонах

---
+++ src/Triangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sideA;
+        int sideB;
+        int sideC;
+        System.out.println("Input sideA");
+        sideA = sc.nextInt();
+        System.out.println("Input sideB");
+        sideB = sc.nextInt();
+        System.out.println("Input sideC");
+        sideC = sc.nextInt();
+        if(sideA+sideB > sideC || sideA+sideC > sideB || sideC+sideB > sideA) {
+            System.out.println("This triangle exists");
+        } else {
+            System.out.println("This triangle doesn't exist");
+        }
+    }
+}
