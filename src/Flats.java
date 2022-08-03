cab2f13e241492692c3a3b8236f21fb8092fd47f
Nikita Plotnitskiy <NikitaPl131@gmail.com>
2022-08-03 17:16:50 +0300

    Программа, которая считывает с клавиатуры, в каком подъезде и на каком этаже расположен номер квартиры

---
+++ src/Flats.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+public class Flats {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int flatNumber;
+    System.out.println("Input flatNumber");
+    flatNumber = sc.nextInt();
+    if (flatNumber<=4) {
+        System.out.println("Entrance: 1" + ", " + "floor: 1");
+    }else if (flatNumber<=8){
+        System.out.println("Entrance: 1" + ", " + "floor: 2");
+    }else if (flatNumber<=12) {
+        System.out.println("Entrance: 1" + ", " + "floor: 3");
+    }else if (flatNumber<=16) {
+        System.out.println("Entrance: 1" + ", " + "floor: 4");
+    }else if (flatNumber<=20) {
+        System.out.println("Entrance: 1" + ", " + "floor: 5");
+    }else if (flatNumber<=24) {
+        System.out.println("Entrance: 1" + ", " + "floor: 6");
+    }else if (flatNumber<=28) {
+        System.out.println("Entrance: 1" + ", " + "floor: 7");
+    }else if (flatNumber<=32) {
+        System.out.println("Entrance: 1" + ", " + "floor: 8");
+    }else if (flatNumber<=36) {
+        System.out.println("Entrance: 1" + ", " + "floor: 9");
+    }else if (flatNumber<=40) {
+        System.out.println("Entrance: 2" + ", " + "floor: 1");
+    }else if (flatNumber<=44) {
+        System.out.println("Entrance: 2" + ", " + "floor: 2");
+    }else if (flatNumber<=48) {
+        System.out.println("Entrance: 2" + ", " + "floor: 3");
+    }else if (flatNumber<=52) {
+        System.out.println("Entrance: 2" + ", " + "floor: 4");
+    }else if (flatNumber<=56) {
+        System.out.println("Entrance: 2" + ", " + "floor: 5");
+    }else if (flatNumber<=60) {
+        System.out.println("Entrance: 2" + ", " + "floor: 6");
+    }else if (flatNumber<=64) {
+        System.out.println("Entrance: 2" + ", " + "floor: 7");
+    }else if (flatNumber<=68) {
+        System.out.println("Entrance: 2" + ", " + "floor: 8");
+    }else if (flatNumber<=70) {
+        System.out.println("Entrance: 2" + ", " + "floor: 9");
+    }else if (flatNumber<=74) {
+        System.out.println("Entrance: 3" + ", " + "floor: 1");
+    }else if (flatNumber<=78) {
+        System.out.println("Entrance: 3" + ", " + "floor: 2");
+    }else if (flatNumber<=82) {
+        System.out.println("Entrance: 3" + ", " + "floor: 3");
+    }else if (flatNumber<=86) {
+        System.out.println("Entrance: 3" + ", " + "floor: 4");
+    }else if (flatNumber<=90) {
+        System.out.println("Entrance: 3" + ", " + "floor: 5");
+    }else if (flatNumber<=94) {
+        System.out.println("Entrance: 3" + ", " + "floor: 6");
+    }else if (flatNumber<=98) {
+        System.out.println("Entrance: 3" + ", " + "floor: 7");
+    }else if (flatNumber<=102) {
+        System.out.println("Entrance: 3" + ", " + "floor: 8");
+    }else if (flatNumber<=106) {
+        System.out.println("Entrance: 3" + ", " + "floor: 9");
+    }else if (flatNumber<=110) {
+        System.out.println("Entrance: 4" + ", " + "floor: 1");
+    }else if (flatNumber<=114) {
+        System.out.println("Entrance: 4" + ", " + "floor: 2");
+    }else if (flatNumber<=118) {
+        System.out.println("Entrance: 4" + ", " + "floor: 3");
+    }else if (flatNumber<=120) {
+        System.out.println("Entrance: 4" + ", " + "floor: 4");
+    }else if (flatNumber<=124) {
+        System.out.println("Entrance: 4" + ", " + "floor: 5");
+    }else if (flatNumber<=128) {
+        System.out.println("Entrance: 4" + ", " + "floor: 6");
+    }else if (flatNumber<=130) {
+        System.out.println("Entrance: 4" + ", " + "floor: 7");
+    }else if (flatNumber<=134) {
+        System.out.println("Entrance: 4" + ", " + "floor: 8");
+    }else if (flatNumber<=138) {
+        System.out.println("Entrance: 4" + ", " + "floor: 9");
+    }else{
+        System.out.println("There is no flat in the building");
+    }
+    }
+}
