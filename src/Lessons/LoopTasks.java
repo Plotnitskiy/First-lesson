9475a0288dc71e483c9939ed54d536b771270adf
Nikita Plotnitskiy <NikitaPl131@gmail.com>
2022-08-06 21:44:45 +0300

    My java practice

--- src/Lessons/LoopTasks.java
+++ src/Lessons/LoopTasks.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class LoopTasks {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int numberA;
-        int numberB;
         int yesOrNot;
         do{
         System.out.println("Input numberA");
-        numberA = scanner.nextInt();
+        int numberA = scanner.nextInt();
         System.out.println("Input numberB");
-        numberB = scanner.nextInt();
+        int numberB = scanner.nextInt();
         System.out.println(numberA * numberB);
         System.out.println("once again?");
         yesOrNot = scanner.nextInt();
