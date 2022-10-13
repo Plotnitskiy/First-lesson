de5d71e14266524ccce2d6f9d719a1093800538a
Nikita Plotnitskiy <NikitaPl131@gmail.com>
2022-10-13 09:47:16 +0300

    ProgAcademy Lesson #1

--- src/Lessons/MyName.java
+++ src/Lessons/MyName.java
@@ -2,7 +2,18 @@ package Lessons;
 
 import java.util.Scanner;
 
+/**
+ * Program that print a string
+ * @autor Nikita Plotnitskiy
+ * @version java 8.0.1
+ */
+
 public class MyName {
+    /**
+     * Start point.
+     *
+     * @param args command line args
+     */
   public static void main(String [] args) {
   System.out.println("My name is Nikita Plotnitskiy");
       Scanner sc = new Scanner(System.in);
